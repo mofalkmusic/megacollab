c3a25141ef5e5bf91cd09dee97dffb6e2521bfc0
mofalkmusic <promo@mofalk.com>
2025-12-21 23:32:42 +0100

    build: build now works, added needed fix for vue 3.5 import reoslution issue...

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+// WHY DID THIS BREAK IN VUE3.5?????
+declare module '*.vue' {
+	import { DefineComponent } from 'vue'
+	// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+	const component: DefineComponent<{}, {}, any>
+	export default component
+}
